1f263e93fa3d09179925d1b0c1faf45de98ade85
Nelson Estrada <nelsonestrada5@gmail.com>
2023-07-19 18:13:38 -0400

    Making tables easier to read/follow
    the acronyms where making it hard for me lol

--- sql/failed_transactions.sql
+++ sql/failed_transactions.sql
@@ -1,16 +1,16 @@
 SELECT
-  to_timestamp(sc.created) AT time zone 'America/New_York' AS timestamp_est,
-  sc.currency,
-  sc.amount,
-  sc.amount::decimal / 100 AS charge_amount_in_dollars,
-  sc.receipt_email,
-  sc.customer_id,
-  sc.description AS charge_description,
-  sc.invoice_id,
-  sc.id,
-  sp.nickname
-FROM stripe_charges AS sc
-LEFT JOIN stripe_invoice_items AS sii ON sc.invoice_id = sii.invoice_id
-LEFT JOIN stripe_prices AS sp ON sii.price_id = sp.id
-WHERE sc.captured = false -- filter out uncaptured charges
+  to_timestamp(charges.created) AT time zone 'America/New_York' AS timestamp_est,
+  charges.currency,
+  charges.amount,
+  charges.amount::decimal / 100 AS charge_amount_in_dollars,
+  charges.receipt_email,
+  charges.customer_id,
+  charges.description AS charge_description,
+  charges.invoice_id,
+  charges.id,
+  prices.nickname
+FROM stripe_charges AS charges
+LEFT JOIN stripe_invoice_items AS invoice_items ON charges.invoice_id = invoice_items.invoice_id
+LEFT JOIN stripe_prices AS prices ON invoice_items.price_id = prices.id
+WHERE charges.captured = false -- filter out uncaptured charges
 ORDER BY timestamp_est DESC;
